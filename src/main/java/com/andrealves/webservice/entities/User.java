cfc9bfafdcebbd9cd7d331c3f46fcc13c9fd49a8
andrealvesdc <andrealvesdc@gmail.com>
2020-05-09 15:55:32 -0300

    H2 database, teste JPA

--- src/main/java/com/andrealves/webservice/entities/User.java
+++ src/main/java/com/andrealves/webservice/entities/User.java
@@ -2,9 +2,17 @@ package com.andrealves.webservice.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
