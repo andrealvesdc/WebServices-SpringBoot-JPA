09f0cb6988209829591bd084cbf6d9d97af158e6
andrealvesdc <andrealvesdc@gmail.com>
2020-05-09 15:06:55 -0300

    Arquivos iniciais

---
+++ src/main/java/com/andrealves/webservice/WebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.andrealves.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceApplication.class, args);
+		// teste
+	}
+
+}
