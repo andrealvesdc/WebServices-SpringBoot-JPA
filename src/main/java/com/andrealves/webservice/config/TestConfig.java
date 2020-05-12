3983422cff42e8320a97aff7fdf0b92533e22d89
andrealvesdc <andrealvesdc@gmail.com>
2020-05-12 20:41:50 -0300

    Payment, one-to-one association

--- src/main/java/com/andrealves/webservice/config/TestConfig.java
+++ src/main/java/com/andrealves/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.andrealves.webservice.entities.Category;
 import com.andrealves.webservice.entities.Order;
 import com.andrealves.webservice.entities.OrderItem;
+import com.andrealves.webservice.entities.Payment;
 import com.andrealves.webservice.entities.Product;
 import com.andrealves.webservice.entities.User;
 import com.andrealves.webservice.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
