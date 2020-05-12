a6899b9f9b260de56d5a725a30044d14616e7aa7
andrealvesdc <andrealvesdc@gmail.com>
2020-05-11 21:27:23 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/andrealves/webservice/entities/OrderItem.java
+++ src/main/java/com/andrealves/webservice/entities/OrderItem.java
@@ -40,6 +40,7 @@ public class OrderItem implements Serializable{
 		id.setOrder(order);
 	}
 	
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
--- src/main/java/com/andrealves/webservice/entities/Product.java
+++ src/main/java/com/andrealves/webservice/entities/Product.java
@@ -11,9 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -32,6 +35,9 @@ public class Product implements Serializable{
 	@JoinTable(name = "tb_prosuct_category", joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product(){
 	}
@@ -87,6 +93,15 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
