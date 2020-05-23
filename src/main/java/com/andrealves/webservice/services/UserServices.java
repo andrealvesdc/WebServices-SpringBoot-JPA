190d36b28b90aa850a7da5cccf6557497a17947d
André Alves <andrealvesdc@gmail.com>
2020-05-23 14:56:44 -0300

    Exception handling - update

--- src/main/java/com/andrealves/webservice/services/UserServices.java
+++ src/main/java/com/andrealves/webservice/services/UserServices.java
@@ -3,6 +3,8 @@ package com.andrealves.webservice.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +47,14 @@ public class UserServices {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
