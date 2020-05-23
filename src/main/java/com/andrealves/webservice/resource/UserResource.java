26b1662756d6079d95a74dff3855dd070ea989cb
André Alves <andrealvesdc@gmail.com>
2020-05-23 10:40:54 -0300

    User insert

--- src/main/java/com/andrealves/webservice/resource/UserResource.java
+++ src/main/java/com/andrealves/webservice/resource/UserResource.java
@@ -1,13 +1,20 @@
 package com.andrealves.webservice.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.sql.rowset.serial.SerialArray;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andrealves.webservice.entities.User;
 import com.andrealves.webservice.services.UserServices;
@@ -31,6 +38,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri	= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
--- src/main/java/com/andrealves/webservice/services/UserServices.java
+++ src/main/java/com/andrealves/webservice/services/UserServices.java
@@ -23,5 +23,9 @@ public class UserServices {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 
 }
