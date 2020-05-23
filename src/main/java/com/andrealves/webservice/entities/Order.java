55c173afedd36d56b286417f86c732e78bc0ff10
80718147 <aalves@indracompany.com>
2020-05-22 21:48:46 -0300

    SubTotal & total methods

--- src/main/java/com/andrealves/webservice/entities/Order.java
+++ src/main/java/com/andrealves/webservice/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getSubTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
