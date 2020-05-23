737aad732c0854351e16cb25521210a0dd087ae9
André Alves <andrealvesdc@gmail.com>
2020-05-23 11:10:49 -0300

    User delete

--- src/main/java/com/andrealves/webservice/services/UserServices.java
+++ src/main/java/com/andrealves/webservice/services/UserServices.java
@@ -25,7 +25,11 @@ public class UserServices {
 	}
 	
 	public User insert(User obj) {
-		return repository.save(obj);
+		return repository.save(obj); 
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 
 }
