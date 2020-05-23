2afa5f978242a188493b1d3386ee3e62fda5d0f6
André Alves <andrealvesdc@gmail.com>
2020-05-23 14:38:44 -0300

    Exception handleing - delete

--- src/main/java/com/andrealves/webservice/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/andrealves/webservice/resource/exception/ResourceExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.ResourceAccessException;
 
+import com.andrealves.webservice.services.exception.DatabaseException;
 import com.andrealves.webservice.services.exception.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request ){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request ){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/andrealves/webservice/services/UserServices.java
+++ src/main/java/com/andrealves/webservice/services/UserServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.andrealves.webservice.entities.User;
 import com.andrealves.webservice.repositories.UserRepository;
+import com.andrealves.webservice.services.exception.DatabaseException;
 import com.andrealves.webservice.services.exception.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserServices {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
